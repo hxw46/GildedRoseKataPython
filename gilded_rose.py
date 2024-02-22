07d3703a23aed10d6d59e77b6ea2a57d819faaf3
edwin <hxw543551500@163.com>
2024-02-21 19:06:55 -0800

    lab4

--- gilded_rose.py
+++ gilded_rose.py
@@ -13,17 +13,19 @@ class Item:
 
 
 class GildedRose(object):
-
     def __init__(self, items: list[Item]):
         # DO NOT CHANGE THIS ATTRIBUTE!!!
         self.items = items
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert" and item.name != "Sulfuras, Hand of Ragnaros":
+                if "Conjured" in item.name:  # 检查商品是否是Conjured类型
+                    degrade_rate = 2  # Conjured商品的质量下降速度是普通商品的两倍
+                else:
+                    degrade_rate = 1  # 普通商品的质量下降速度
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+                    item.quality = item.quality - degrade_rate
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
@@ -37,13 +39,15 @@ class GildedRose(object):
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
+                if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+                    if "Conjured" in item.name:  # 检查商品是否过期且是Conjured类型
+                        degrade_rate = 2  # Conjured商品过期后的质量下降速度加倍
                     else:
-                        item.quality = item.quality - item.quality
+                        degrade_rate = 1  # 普通商品过期后的质量下降速度
+                    if item.quality > 0:
+                        item.quality = item.quality - degrade_rate
+                elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                    item.quality = 0
                 else:
                     if item.quality < 50:
                         item.quality = item.quality + 1
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -5,11 +5,52 @@ from gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+    def test_vest_item_should_decrease_after_one_day(self):
+        vest = "+5 Dexterity Vest"
+        items = [Item(vest, 1, 2), Item(vest, 9, 19), Item(vest, 4, 6)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+
+        expected_results = [(0, 1), (8, 18), (3, 5)]
+        
+        for item, (expected_sell_in, expected_quality) in zip(items, expected_results):
+            self.assertEqual(item.sell_in, expected_sell_in)
+            self.assertEqual(item.quality, expected_quality)
+
+    def test_conjured_item_degrades_twice_as_fast(self):
+        conjured_item = "Conjured Mana Cake"
+        items = [Item(conjured_item, 3, 6)]
+        gr = GildedRose(items)
+
+        gr.update_quality()
+        
+        expected_quality = 4
+        self.assertEqual(items[0].quality, expected_quality)
+    
+    def test_aged_brie_increases_in_quality_over_time(self):
+        items = [Item("Aged Brie", 2, 0)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        self.assertEqual("Aged Brie", items[0].name)
+        self.assertEqual(1, items[0].sell_in)
+        self.assertEqual(1, items[0].quality)
+
+    def test_backstage_passes_increases_in_quality_as_sell_in_approaches(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        self.assertEqual("Backstage passes to a TAFKAL80ETC concert", items[0].name)
+        self.assertEqual(14, items[0].sell_in)
+        self.assertEqual(21, items[0].quality)
+
+    def test_sulfuras_never_decreases_in_quality_or_sell_in(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 80)]
+        gr = GildedRose(items)
+        gr.update_quality()
+        self.assertEqual("Sulfuras, Hand of Ragnaros", items[0].name)
+        self.assertEqual(0, items[0].sell_in) 
+        self.assertEqual(80, items[0].quality)
 
 
 if __name__ == '__main__':
